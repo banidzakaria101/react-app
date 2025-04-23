38cf1ed2b790348811552afc50005bd07f4ef65f
zakariabanid101 <banidzakaria2@gmail.com>
2025-04-23 16:47:07 +0100

    1

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,27 @@
 
 function ListGroup() {
+  const items = [
+    'New York',
+    'San Fransisco',
+    'Tokyo',
+    'London',
+    'Paris'
+  ];
+
+
+
+
   return (
     <>
-        <h1>List</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A fourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>List</h1>
+      {items.length === 0 && <p>nothing to display</p>}
+      <ul className="list-group">
+        {items.map((item, index) =>
+          <li className="list-group-item" 
+          key={item} onClick={() => 
+          console.log(item, index)}>{item}</li>)}
+
+      </ul>
     </>
   );
 }
